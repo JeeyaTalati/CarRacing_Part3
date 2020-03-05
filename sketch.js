90b4bda891bd37c72ec3c4845437c5a9330fb4ee
JeeyaTalati <2235.stkabirnrnp@gmail.com>
2020-03-05 18:52:52 +0530

    we added the images.

--- sketch.js
+++ sketch.js
@@ -9,7 +9,14 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
-
+var track ,carImage1,carImage2,carImage3,carImage4;
+function preload (){
+track=loadImage("track.jpg");
+carImage1=loadImage("car1.png");
+carImage2=loadImage("car2.png");
+carImage3=loadImage("car3.png");
+carImage4=loadImage("car4.png");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -27,5 +34,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if (gameState===2){
+  game.end();
   }
 }
